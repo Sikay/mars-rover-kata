4489952c7e12fe9836bac84be2b3768b7b2455c8
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-11 13:21:12 +0200

    changed names

---
+++ php/src/MarsRover.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MarsRover;
+
+class MarsRover
+{
+    public function __construct(Grid $grid)
+    {
+
+    }
+
+    public function execute(String $command): string
+    {
+
+    }
+}
---
+++ php/tests/MarsRoverTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MarsRover\Test;
+
+use MarsRover\MarsRover;
+use PHPUnit\Framework\TestCase;
+
+class MarsRoverTest extends TestCase
+{
+    /** @test */
+    public function change_me()
+    {
+        $this->assertTrue(true);
+    }
+}
