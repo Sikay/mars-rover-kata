c88f7f5eccedc60674ef93867df0d2b3475d1ff2
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-11 18:27:54 +0200

    refactor with data provider

--- php/tests/DirectionTest.php
+++ php/tests/DirectionTest.php
@@ -4,20 +4,43 @@ namespace MarsRover\Test;
 
 use MarsRover\Direction;
 use PHPUnit\Framework\TestCase;
+use Iterator;
 
 class DirectionTest extends TestCase
 {
-    /** @test */
-    public function should_create_direction_with_valid_value()
+    public function validDirectionsProvider(): iterator
     {
-        $direction = new Direction('N');
-        $this->assertTrue($direction->value() === 'N');
+        yield "North"   => ['N'];
+        yield "East"    => ['E'];
+        yield "South"   => ['S'];
+        yield "West"    => ['W'];
     }
 
-    /** @test */
-    public function should_not_create_direction_with_invalid_value()
+    /**
+     * @test
+     * @dataProvider validDirectionsProvider
+     */
+    public function should_create_direction_with_valid_value($validDirection)
+    {
+        $direction = new Direction($validDirection);
+        $this->assertTrue($direction->value() === $validDirection);
+    }
+
+    public function invalidDirectionsProvider(): iterator
+    {
+        yield "Invalid direction A" => ['A'];
+        yield "Invalid direction C" => ['C'];
+        yield "Invalid direction H" => ['H'];
+        yield "Invalid direction P" => ['P'];
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidDirectionsProvider
+     */
+    public function should_not_create_direction_with_invalid_value($invalidDirection)
     {
         $this->expectException(\InvalidArgumentException::class);
-        $direction = new Direction('A');
+        $direction = new Direction($invalidDirection);
     }
 }
