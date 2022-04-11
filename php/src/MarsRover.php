a88184b70b30230aabf61f3ac1ab167e97ce2bc8
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-11 14:49:15 +0200

    refactor turn direcction in two functions

--- php/src/MarsRover.php
+++ php/src/MarsRover.php
@@ -17,24 +17,12 @@ class MarsRover
 
         $charCommands = str_split($command);
         foreach ($charCommands as $charCommand) {
-            if ($charCommand === 'R' && $direction === 'N') {
-                $direction = 'E';
-            } else if ($charCommand === 'R' && $direction === 'E') {
-                $direction = 'S';
-            } else if ($charCommand === 'R' && $direction === 'S') {
-                $direction = 'W';
-            } else if ($charCommand === 'R' && $direction === 'W') {
-                $direction = 'N';
+            if ($charCommand === 'R') {
+                $direction = $this->turnRight($direction);
             }
 
-            if ($charCommand === 'L' && $direction === 'E') {
-                $direction = 'N';
-            } else if ($charCommand === 'L' && $direction === 'N') {
-                $direction = 'W';
-            } else if ($charCommand === 'L' && $direction === 'W') {
-                $direction = 'S';
-            } else if ($charCommand === 'L' && $direction === 'S') {
-                $direction = 'E';
+            if ($charCommand === 'L') {
+                $direction = $this->turnLeft($direction);
             }
 
             if ($charCommand === 'M' && $direction === 'N') {
@@ -49,5 +37,37 @@ class MarsRover
         }
 
         return $x . ':' . $y . ':' . $direction;
+    }
+
+    public function turnRight(string $direction): string
+    {
+        $rightDirection = '';
+        if ($direction === 'N') {
+            $rightDirection = 'E';
+        } else if ($direction === 'E') {
+            $rightDirection = 'S';
+        } else if ($direction === 'S') {
+            $rightDirection = 'W';
+        } else if ($direction === 'W') {
+            $rightDirection = 'N';
+        }
+
+        return $rightDirection;
+    }
+
+    public function turnLeft(string $direction): string
+    {
+        $leftDirection = '';
+        if ($direction === 'E') {
+            $leftDirection = 'N';
+        } else if ($direction === 'N') {
+            $leftDirection = 'W';
+        } else if ($direction === 'W') {
+            $leftDirection = 'S';
+        } else if ($direction === 'S') {
+            $leftDirection = 'E';
+        }
+
+        return $leftDirection;
     }
 }
