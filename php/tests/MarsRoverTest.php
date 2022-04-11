95846aca946c69f937a056b5ec479821bcb28668
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-11 14:36:58 +0200

    refactor test with provider

--- php/tests/MarsRoverTest.php
+++ php/tests/MarsRoverTest.php
@@ -5,6 +5,7 @@ namespace MarsRover\Test;
 use MarsRover\MarsRover;
 use MarsRover\Grid;
 use PHPUnit\Framework\TestCase;
+use Iterator;
 
 class MarsRoverTest extends TestCase
 {
@@ -78,5 +79,29 @@ class MarsRoverTest extends TestCase
         $grid = new Grid();
         $marsRover = new MarsRover($grid);
         $this->assertTrue($marsRover->execute('MMRMMRMRMRMM') === '1:3:N');
+    }
+
+    public function validCommandsProvider(): iterator
+    {
+        yield "should_move_forward"                                     => ['M', '0:1:N'];
+        yield "should_move_forward_two_positions"                       => ['MM', '0:2:N'];
+        yield "should_move_forward_turn_right_and_continue"             => ['MMRMM', '2:2:E'];
+        yield "should_move_forward_turn_left_continue_and_turn_left"    => ['MMRMMLM', '2:3:N'];
+        yield "should_move_forward_and_turn_to_west"                    => ['MMRMMLMLM', '1:3:W'];
+        yield "should_move_forward_and_turn_left_to_west"               => ['MMRMMLMLM', '1:3:W'];
+        yield "should_move_forward_and_turn_left_to_south"              => ['MMRMMLMLMLM', '1:2:S'];
+        yield "should_move_forward_and_turn_left_to_complete_circle"    => ['MMRMMLMLMLMLM', '2:2:E'];
+        yield "should_move_forward_and_turn_right_to_complete_circle"   => ['MMRMMRMRMRMM', '1:3:N'];
+    }
+
+    /**
+     * @test
+     * @dataProvider validCommandsProvider
+     */
+    public function should_move_to_destination(string $command, string $destination): void
+    {
+        $grid = new Grid();
+        $marsRover = new MarsRover($grid);
+        $this->assertTrue($marsRover->execute($command) === $destination);
     }
 }
