330b5dac48e9a7d64b175c6d4c1c26bcf1924eef
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-11 16:51:59 +0200

    should_return_same_y_coordinate

--- php/src/Coordinate.php
+++ php/src/Coordinate.php
@@ -9,10 +9,16 @@ class Coordinate
     public function __construct(int $x, int $y)
     {
         $this->x = $x;
+        $this->y = $y;
     }
 
     public function x()
     {
         return $this->x;
+    }
+
+    public function y()
+    {
+        return $this->y;
     }
 }
