0797cefba17f5583a39d7945419be61f32c59ea1
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-14 19:06:50 +0200

    should_exceed_the_limit_width

--- php/src/Grid.php
+++ php/src/Grid.php
@@ -4,5 +4,18 @@ namespace MarsRover;
 
 class Grid
 {
+    private const MAX_WIDTH = 10;
 
+    public function maxWidth(): int
+    {
+        return self::MAX_WIDTH;
+    }
+
+    public function exceedLimit(Coordinate $coordinate): bool
+    {
+        if ($coordinate->x() >= self::MAX_WIDTH) {
+            return true;
+        }
+        return false;
+    }
 }
---
+++ php/tests/GridTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MarsRover\Test;
+
+use MarsRover\Grid;
+use MarsRover\Coordinate;
+use PHPUnit\Framework\TestCase;
+
+class GridTest extends TestCase
+{
+    /** @test */
+    public function should_exceed_the_limit_width()
+    {
+        $grid = new Grid();
+        $coordinate = new Coordinate($grid->maxWidth() + 1, 0);
+        $this->assertTrue($grid->exceedLimit($coordinate), true);
+    }
+}
